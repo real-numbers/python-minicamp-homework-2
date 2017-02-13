ec1b580a60b33b53f9429967df36ee56ac3e57ea
real-numbers <real_numbers@comcast.net>
2017-02-13 11:35:17 -0800

    Sum, Product, Difference routes done

--- app.py
+++ app.py
@@ -7,7 +7,15 @@ def index():
 	
 @app.route('/sum/<int1>/<int2>')
 def theSum(int1,int2):
-	return "The sum is " + str(int(int1) + int(int2))
+	return str(int1) + " + " + str(int2) + " = " + str(int(int1) + int(int2))
+	
+@app.route('/product/<int1>/<int2>')
+def theProduct(int1,int2):
+	return str(int1) + " x " + str(int2) + " = " + str(int(int1) * int(int2))
+	
+@app.route('/difference/<int1>/<int2>')
+def theDifference(int1,int2):
+	return str(int1) + " - " + str(int2) + " = " + str(int(int1) - int(int2))
 
 @app.route('/birthday')
 def birthday():
