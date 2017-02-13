a5f6f774048ab0fa735625de4b5a00b998cb8f4c
real-numbers <real_numbers@comcast.net>
2017-02-13 12:20:24 -0800

    updated requestexample.py

--- RequestExample.py
+++ RequestExample.py
@@ -4,12 +4,23 @@ r1 = requests.get(url_1)
 
 print(r1.text)
 
-url_2 = "http://localhost:5000/"
-r2 = requests.get(url_2)
-
-print(r2.text)
-
 url_3 = "http://localhost:5000/birthday"
 r3 = requests.get(url_3)
 
 print(r3.text)
+
+url_4 = "http://localhost:5000/sum/3/5"
+r4 = requests.get(url_4)
+
+print(r4.text)
+
+url_5 = "http://localhost:5000/product/3/5"
+r5 = requests.get(url_5)
+
+print(r5.text)
+
+url_6 = "http://localhost:5000/favoritefoods"
+r6 = requests.get(url_6)
+
+print(r6.text)
+
